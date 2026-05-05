85a37e09883a244ab5a1f589e33f4da1898a0eb7
Andres0513 <yukaifeng0513@163.com>
2026-05-05 23:21:26 +0800

    切换tensorflow为gpu版本

--- electricity_price/predict_current_model.py
+++ electricity_price/predict_current_model.py
@@ -1,14 +1,16 @@
 import joblib
 
 # 加载数据
-X_train = joblib.load("X_train.pkl")
-y_train = joblib.load("y_train.pkl")
-X_test = joblib.load("X_test.pkl")
-y_test = joblib.load("y_test.pkl")
+X_train = joblib.load("/Users/yukaifeng/Codes/Python/trail02/electricity_price/X_train_epoch_20_batch_32_input15days_predict_D_D_4.pkl")
+y_train = joblib.load("/Users/yukaifeng/Codes/Python/trail02/electricity_price/y_train_epoch_20_batch_32_input15days_predict_D_D_4.pkl")
+X_test = joblib.load("/Users/yukaifeng/Codes/Python/trail02/electricity_price/X_test_epoch_20_batch_32_input15days_predict_D_D_4.pkl")
+y_test = joblib.load("/Users/yukaifeng/Codes/Python/trail02/electricity_price/y_test_epoch_20_batch_32_input15days_predict_D_D_4.pkl")
 
 # 加载模型和缩放器
-model = joblib.load("price_model_epoch_30_batch_32_input15days_predict_D_D_4.pkl")
-scaler = joblib.load("price_scaler_epoch_30_batch_32_input15days_predict_D_D_4.pkl")
+model = joblib.load("/Users/yukaifeng/Codes/Python/trail02/electricity_price/price_model_epoch_20_batch_32_input15days_predict_D_D_4.pkl")
+scaler = joblib.load("/Users/yukaifeng/Codes/Python/trail02/electricity_price/price_scaler_epoch_20_batch_32_input15days_predict_D_D_4.pkl")
 
 # 直接预测
 y_pred = model.predict(X_test, verbose=0)
+
+a = 0
